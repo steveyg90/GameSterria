2983d7dad3c863b5a4a3e228469bb23957aedc79
steveyg <steve.green@qa.com>
2016-03-26 18:28:41 +0000

    first commit

---
+++ my-gdx-game-core/src/com/mygdx/badguys/OutsideLand.java
@@ -0,0 +1,11 @@
+package com.mygdx.badguys;
+
+// stores x,y positions of possible spawn points for baddies outside
+public class OutsideLand {
+	
+	public int x, y;
+	public OutsideLand(int x, int y) {
+		this.x = x; 
+		this.y = y;
+	}
+}
